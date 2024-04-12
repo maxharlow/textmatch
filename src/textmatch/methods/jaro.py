f730eb7b41a93b558fa1fc53dbf3cda3edbec09b
Max Harlow <maxharlow@gmail.com>
2024-04-12 12:05:28 +0100

    Removed argument

--- src/textmatch/methods/jaro.py
+++ src/textmatch/methods/jaro.py
@@ -3,4 +3,4 @@ import polars
 import polars_distance
 
 def compare(data: PolarsDataframe, header1: str, header2: str, header_degree: str) -> PolarsDataframe:
-    return data.with_columns(polars.lit(1).sub(polars_distance.col(header1).dist_str.jaro_winkler(header2, normalized=True)).cast(polars.Float32).alias(header_degree))
+    return data.with_columns(polars.lit(1).sub(polars_distance.col(header1).dist_str.jaro_winkler(header2)).cast(polars.Float32).alias(header_degree))
