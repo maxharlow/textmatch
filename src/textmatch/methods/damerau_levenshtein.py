939c2b6368f227068aafb4b2bd0652fb533d5d4b
Max Harlow <maxharlow@gmail.com>
2026-01-17 21:49:17 +0000

    Switch to Polars-DS

--- src/textmatch/methods/damerau_levenshtein.py
+++ src/textmatch/methods/damerau_levenshtein.py
@@ -1,6 +1,6 @@
 from ..typings import PolarsDataframe
 import polars
-import polars_distance
+import polars_ds
 
 def compare(data: PolarsDataframe, header1: str, header2: str, header_degree: str) -> PolarsDataframe:
-    return data.with_columns(polars.lit(1).sub(polars_distance.col(header1).dist_str.damerau_levenshtein(header2, normalized=True)).cast(polars.Float32).alias(header_degree))
+    return data.with_columns(polars_ds.str_d_leven(polars.col(header1), polars.col(header2), return_sim=True, parallel=True).cast(polars.Float32).alias(header_degree))
--- src/textmatch/methods/jaro_winkler.py
+++ src/textmatch/methods/jaro_winkler.py
@@ -1,6 +1,6 @@
 from ..typings import PolarsDataframe
 import polars
-import polars_distance
+import polars_ds
 
 def compare(data: PolarsDataframe, header1: str, header2: str, header_degree: str) -> PolarsDataframe:
-    return data.with_columns(polars.lit(1).sub(polars_distance.col(header1).dist_str.jaro_winkler(header2)).cast(polars.Float32).alias(header_degree))
+    return data.with_columns(polars_ds.str_jw(polars.col(header1), polars.col(header2), parallel=True).cast(polars.Float32).alias(header_degree))
